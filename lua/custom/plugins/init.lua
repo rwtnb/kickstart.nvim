3cd723c3b6269d1babdc30b8c6caf75af9519db2
Rodrigo Watanabe <1060807+rwtnb@users.noreply.github.com>
2024-07-30 00:01:21 -0300

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,7 +2,7 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-vim.g.ruby_host_prog = '/Users/rodrigo/.gem/ruby/3.3.0/bin/neovim-ruby-host'
+vim.g.ruby_host_prog = '~/.gem/ruby/3.3.0/bin/neovim-ruby-host'
 vim.g.python3_host_prog = '~/.venv/bin/python'
 
 return {
